abcfa782190c2c9e3e0a7358ec464338c8f7701b
Przemysław Pobuta <przemyslaw.pobuta@asseco.pl>
2021-06-20 15:15:48 +0200

    changes

--- apps/gatsby-wordpress-playground/src/pages/index.tsx
+++ apps/gatsby-wordpress-playground/src/pages/index.tsx
@@ -5,15 +5,16 @@ export function Index() {
   return (
     <>
       <h1>Aplikacja testowa</h1>
-      <Button 
-          border="none"
-          color="gray"
-          height = "50px"
-          onClick={() => console.log("You clicked button!")}
-          radius = "5%"
-          width = "300px"
-          children = "Button"
-        />
+      <Button
+        backgroundColor="#3498db"
+        border="2px solid #2980b9"
+        color="#ffffff"
+        height={60}
+        radius={100}
+        width={200}
+      >
+        Przycisk
+      </Button>
     </>
   );
 }
